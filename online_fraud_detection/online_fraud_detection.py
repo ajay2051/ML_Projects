6173e5760378ab6dfe474c7b76b0b87f865d2c7b
ajay🙋 <ajaythk.94@gmail.com>
2025-04-26 14:29:23 +0545

    off

--- online_fraud_detection/online_fraud_detection.py
+++ online_fraud_detection/online_fraud_detection.py
@@ -1,2 +1,20 @@
 import pandas as pd
 import numpy as np
+
+df = pd.read_csv("credit_card.csv")
+df.head()
+
+df.isnull().sum()
+
+df.type.value_counts()
+
+type = df["type"].value_counts()
+transactions = type.index
+quantity = type.values
+
+import plotly.express as px
+figure = px.pie(df,
+             values=quantity,
+             names=transactions,hole = 0.5,
+             title="Distribution of Transaction Type")
+figure.show()
